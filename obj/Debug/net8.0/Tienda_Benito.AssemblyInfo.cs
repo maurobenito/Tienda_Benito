56dff622d8921bca56c7b443944e5e7983fe48d9
maurobenito <141743206+maurobenito@users.noreply.github.com>
2025-07-09 13:52:05 -0300

    15

--- obj/Debug/net8.0/Tienda_Benito.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tienda_Benito.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tienda_Benito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+721788e9dfd58f8714c298125eb55113fd3a4b11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0dee96a7a3bad20c9deb417abdd157c6ed7977")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tienda_Benito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tienda_Benito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
